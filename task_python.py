342d6586b9ff036eb10d527055d415e384f48c08
gereldan <gerel@bk.ru>
2021-06-17 11:49:51 +0500

    update 17.06.2021

--- task_python.py
+++ task_python.py
@@ -1,83 +1,103 @@
-import numpy as np
-
 import requests as req
-
 import json
-
 import pandas as pd
-
 import matplotlib.pyplot as plt
-
 from datetime import datetime
-
 from tokens import *
 
 
-start_date = input('Введите дату (по умолчанию первое число текущего месяца): ')
-if start_date == '':
+def take_start_date(attempt: int = 0):
+    """
+    :return: text start date
+    """
+    attempt += 1
     now = datetime.now()
-    start_date = f'01.{now.month}.{now.year}'
-#Данные для авторизации
+    start_date_text = input('Введите дату в формате дд.мм.гггг (по умолчанию первое число текущего месяца): ')
+    if start_date_text == '':
+        start_date_text = f'01.{now.month}.{now.year}'
+    try:
+        datetime.strptime(start_date_text, '%d.%m.%Y')
+    except Exception:
+        print(f'You must insert date in format dd.mm.YYYY, {5 - attempt} attempt left')
+        if attempt > 4:
+            print('attempts ended the program closes')
+            exit()
+        else:
+            start_date_text = take_start_date(attempt)
+    return start_date_text
 
 
-auth = (email, api_key)
-#Ссылка для запроса
-url = f'https://mycompany.omnidesk.ru/api/cases.json?from_time={start_date}'
-#Заголовок запроса
-headers = {'Content-Type': 'application/json'}
-#Отправляем запрос с нужными данными
-omni = req.get(url=url, headers=headers, auth=auth)
-#Получаем текст ответа
-response = omni.text
-#преобразуем текст ответа в словарь
-json_obj = json.loads(response)
+def get_data(filter_date: str):
+    """
+    :param filter_date: start date filter
+    :return: dictionary
+    """
+    # Данные для запроса и авторизацииавторизации
+    auth = (email, api_key)
+    url = f'https://mycompany.omnidesk.ru/api/cases.json?from_time={filter_date}'
+    headers = {'Content-Type': 'application/json'}
 
-#задаем столбцы для датафрейма
-columns = list(json_obj['0']['case'].keys())
-data = []
-#пробегаемся циклом по всем заявкам
-for i in range(json_obj['total_count']):
-    row = []
-    i_str = str(i)
-    #Добавляем в список значения всех столбцов
-    for key in json_obj[i_str]['case']:
-        row.append(json_obj[i_str]['case'][key])
-    #Делаем список списков для датафрейма
-    data.append(row)
+    # Отправляем запрос
+    omni = req.get(url=url, headers=headers, auth=auth)
+    response = omni.text
+    return json.loads(response)
 
-#создаем датафрейм на основе данных
-df = pd.DataFrame(data=data, columns=columns)
-#Убираем открытые заявки они для анализа в текущий момент не нужны
-df_close_only = df[df['closing_speed'] != '-']
-#Преобразуем в числовое значение столбец с минутами
-df_close_only['closing_speed'] = df_close_only['closing_speed'].astype('int')
-#Зададим гистограмму
-df_close_only['closing_speed'].hist(bins=10)
-#Сохраним гистограмму
+
+def clear_df(dataframe):
+    """
+    :param dataframe: dirty dataframe
+    :return: clear dataframe
+    """
+    # Убираем открытые заявки они для анализа в текущий момент не нужны
+    close_only = dataframe[dataframe['closing_speed'] != '-']
+
+    # Преобразуем в числовое значение столбец с минутами
+    close_only['closing_speed'] = close_only['closing_speed'].astype('int')
+    """
+    Для решения вопроса сортировки по важности необходимо каждую категорию важности пронумеровать
+    для этого создадим словарь самое важное - самая большая цифра
+    """
+    dict_priority = {'low': 1, 'normal': 2, 'high': 3, 'critical': 4}
+
+    # пробежимся по всем вариантам важности и заменим их на соответствующие значения из словаря
+    close_only['priority'] = close_only['priority'].apply(lambda x: dict_priority.get(x))
+    return close_only
+
+
+def create_df(dictionary: dict):
+    """
+    :param dictionary: dictionary from json object
+    :return:
+    """
+    dataframe = pd.DataFrame()
+    for i in range(dictionary['total_count']):
+        dict_new = dictionary[f'{i}']['case']
+        dataframe = dataframe.append(dict_new, ignore_index=True)
+    return dataframe
+
+
+start_date = take_start_date()
+json_obj = get_data(start_date)
+
+df = create_df(json_obj)
+
+df = clear_df(df)
+
+# Зададим гистограмму
+df['closing_speed'].hist(bins=10)
 plt.savefig('fig2.png')
-#Покажем гистограмму
 plt.show()
-"""
-Для решения вопроса сортировки по важности необходимо каждую категорию важности пронумеровать
-для этого создадим словарь самое важное - самая большая цифра
-"""
-dict_priority = {'low': 1, 'normal': 2, 'high': 3, 'critical': 4}
-#Добавим новый столбец под цифровой эквивалент важности
-df_close_only['priority_num'] = ''
-#пробежимся по всем вариантам важности и проставим в последний столбец соответствующие значения
-for i in dict_priority:
-    df_close_only['priority_num'] = np.where(df_close_only['priority'] == i,
-                                             dict_priority[i], df_close_only['priority_num'])
-#Узнаем среднее время закрытия заявки
-mean_time = df_close_only['closing_speed'].mean()
+
 """
 Создадим новый датафрейм в котором будут только плохие заявки (время закрытия выше среднего)
 Здесь я выбрал среднее так как у меня было открыто мало заявок и в этом случае это допустимо
 У Вас возможно есть какой-то норматив времени ответа на заявку, с определенными критериями
 например по важности
 """
-bad_appeal = df_close_only[df_close_only['closing_speed'] > mean_time]
-#Отсортируем по двум столбцам важности и скорости закрытия вверху самые важные и долгие
-bad_appeal = bad_appeal.sort_values(by=['priority_num', 'closing_speed'], ascending=False)
-#Сохраним последний датафрейм в excel
+bad_appeal = df[df['closing_speed'] > df['closing_speed'].mean()]
+
+# Отсортируем по двум столбцам важности и скорости закрытия вверху самые важные и долгие
+bad_appeal = bad_appeal.sort_values(by=['priority', 'closing_speed'], ascending=False)
+
+# Сохраним последний датафрейм в excel
 bad_appeal.to_excel('bad_appeal.xls', sheet_name='bad_appeal', engine='openpyxl')
