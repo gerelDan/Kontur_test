0f23478fdd468c5201d31494c6eddc155cb1fec4
gereldan <gerel@bk.ru>
2021-06-18 10:22:26 +0500

    update 18.06.2021 правки после замечаний

--- sql_script.py
+++ sql_script.py
@@ -10,21 +10,96 @@ import pandas as pd
 #                      r'Trusted_Connection=yes;'
 #                      )
 
+
 def connect_data_base():
     return pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
                           r'DBQ=C:\Users\yermodan\PycharmProjects\Kontur\Database51.accdb;'
                           )
 
-def get_table(query, cursor, filter_sql, now):
-    table_from_sql = cursor.execute(query, filter_sql, now)
+
+def get_table(query, cur, filtration, date_now):
+    table_from_sql = cur.execute(query, filtration, date_now)
     data = []
-    for i in table_from_sql.fetchall():
-        data.append(list(i))
+    for row in table_from_sql.fetchall():
+        data.append(list(row))
     desc = table_from_sql.description
-    columns = []
+    columns_table = []
     for row in desc:
-        columns.append((row[0]))
-    return data, columns
+        columns_table.append((row[0]))
+    return data, columns_table
+
+
+def work(df_open, df_close):
+    # Проссумируем стомость поставок по счетам
+    df_open = df_open.groupby(
+        ['num', 'bdate', 'pdate', 'cid', 'upto'], as_index=False).agg({'cost': 'sum', 'payed': 'sum'})
+    df_close = df_close.groupby(
+        ['num', 'bdate', 'pdate', 'cid', 'upto'], as_index=False).agg({'cost': 'sum', 'payed': 'sum'})
+
+    # Оставим только такие счета где срок поставки максимальный
+    df_more_now_grouped = df_open.groupby(
+        "cid", group_keys=False).apply(lambda cid: cid.nlargest(1, "upto"))
+    df_more_now_grouped = df_more_now_grouped[['cid', 'num', 'bdate', 'pdate', 'cost', 'payed', 'upto']]
+
+    # Оставим только такие счета где дата оплаты максимальная
+    df_less_now_grouped = df_close.groupby("cid", group_keys=False).apply(lambda cid: cid.nlargest(1, "pdate"))
+
+    # соединим две таблицы
+    df_all = pd.concat([df_more_now_grouped, df_less_now_grouped], axis=0, ignore_index=True)
+
+    # Еще раз оставим только такие счета где срок поставки максимальный
+    df_all = df_all.groupby("cid", group_keys=False).apply(lambda cid: cid.nlargest(1, "upto"))
+
+    # отсортируем по номерам клиентов
+    df_all = df_all.sort_values('cid', ignore_index=True)
+    df_all = df_all[['cid', 'num', 'bdate', 'pdate', 'cost', 'payed']]
+    return df_all
+
+
+def delete_table(cur, connect):
+    x = input('Пожалуйста удостоверьтесь, что в таблице с названием Result_kontur_ekstern нет критически важных данных'
+              'если это так введите "Y" и нажмите enter:')
+    if x.lower() == 'y':
+        try:
+            cur.execute('Drop table Result_kontur_ekstern')
+            connect.commit()
+        except pyodbc.ProgrammingError:
+            pass
+        except pyodbc.Error:
+            print('таблица Result_kontur_ekstern открыта пользователем закройте таблицу')
+            exit()
+    else:
+        print('Вы не подтвердили отсутствие важных данных в таблице программа закрывается')
+        exit()
+
+
+def create_table(col, cur, connect):
+    create_query = f'create table Result_kontur_ekstern (' \
+                   f'{col[0]} int,' \
+                   f' {col[1]} int,' \
+                   f' {col[2]} datetime,' \
+                   f' {col[3]} datetime,' \
+                   f' {col[4]} money,' \
+                   f' {col[5]} money)'
+    cur.execute(create_query)
+    connect.commit()
+
+
+def fill_table (data, col, cur, connect):
+
+# Преобразуем данные в подходящиц формат
+    for i in range(len(data)):
+        data[i][2] = str(data[i][2])[:10]
+        data[i][3] = str(data[i][3])[:10]
+        insert_execute = f'insert into Result_kontur_ekstern (' \
+                         f'{col[0]}, {col[1]}, {col[2]}, {col[3]}, {col[4]}, {col[5]})' \
+                         f'values ' \
+                         f'({data[i][0]}, {data[i][1]}, {data[i][2]},' \
+                         f' {data[i][3]}, {data[i][4]}, {data[i][5]})'
+        insert_execute = str(insert_execute)
+        cur.execute(insert_execute)
+        connect.commit()
+
 
 
 conn = connect_data_base()
@@ -41,90 +116,27 @@ query_close_now = 'select num, bdate, pdate, cid, product, cost, payed, upto, ti
             '(retail_packs.bcID = Bill_content.bcID)) AS query_1 ON (query_1.bID = Bills.id)' \
             'where product = ? and upto is not NULL and upto < ?'
 
-# определяем время сейчас
 now = datetime.now()
 filter_sql = 'Контур-экстерн'
 
-data_open_now = get_table(query_open_now, cursor, filter_sql, now)[0]
-columns = get_table(query_open_now, cursor, filter_sql, now)[1]
-
+data_open_now, columns = get_table(query_open_now, cursor, filter_sql, now)
 data_close_now = get_table(query_close_now, cursor, filter_sql, now)[0]
 
-# С оздадим датафреймы
 df_open_now = pd.DataFrame(data=data_open_now, columns=columns)
 
 df_close_now = pd.DataFrame(data=data_close_now, columns=columns)
+df_finish = work(df_open_now, df_close_now)
 
-# Проссумируем стомость поставок по счетам
-df_open_now = df_open_now.groupby(
-    ['num', 'bdate', 'pdate', 'cid', 'upto'], as_index=False).agg({'cost': 'sum', 'payed': 'sum'})
-df_close_now = df_close_now.groupby(
-    ['num', 'bdate', 'pdate', 'cid', 'upto'], as_index=False).agg({'cost': 'sum', 'payed': 'sum'})
+columns = df_finish.columns
 
-# Оставим только такие счета где срок поставки максимальный
-df_more_now_grouped = df_open_now.groupby(
-    "cid", group_keys=False).apply(lambda x: x.nlargest(1, "upto"))
-df_more_now_grouped = df_more_now_grouped[['cid', 'num', 'bdate', 'pdate', 'cost', 'payed', 'upto']]
+list_data = df_finish.values.tolist()
 
-# Оставим только такие счета где дата оплаты максимальная
-df_less_now_grouped = df_close_now.groupby("cid", group_keys=False).apply(lambda x: x.nlargest(1, "pdate"))
+df_finish.to_excel('result.xls', sheet_name='result', engine='openpyxl')
 
-# соединим две таблицы
-df_all = pd.concat([df_more_now_grouped, df_less_now_grouped], axis=0, ignore_index=True)
+delete_table(cursor, conn)
 
-# Еще раз оставим только такие счета где срок поставки максимальный
-df_all = df_all.groupby("cid", group_keys=False).apply(lambda x: x.nlargest(1, "upto"))
+create_table(columns, cursor, conn)
 
-# отсортируем по номерам клиентов
-df_all = df_all.sort_values('cid', ignore_index=True)
-df_all = df_all[['cid', 'num', 'bdate', 'pdate', 'cost', 'payed']]
+fill_table(list_data, columns, cursor, conn)
 
-# Получим столбцы из датафрейма
-columns = df_all.columns
-
-# Из датафрейма получим список списков
-list_data = df_all.values.tolist()
-
-# Закинем ексель результат
-df_all.to_excel('result.xls', sheet_name='result', engine='openpyxl')
-
-'''
-Здесь результат отправим в базу данных
-Попробуем удалить таблицу
-'''
-x = input('Пожалуйста удостоверьтесь, что в таблице с названием Result_kontur_ekstern нет критически важных данных'
-          'если это так введите "Y" и нажмите enter:')
-if x.lower() == 'y':
-    try:
-        cursor.execute('Drop table Result_kontur_ekstern')
-        conn.commit()
-    except pyodbc.ProgrammingError:
-        pass
-    except pyodbc.Error:
-        print('таблица Result_kontur_ekstern открыта пользователемб закройте таблицу')
-        exit()
-
-# Создадим таблицу заново с нужными названиями и форматами
-    create_query = f'create table Result_kontur_ekstern (' \
-                   f'{columns[0]} int,' \
-                   f' {columns[1]} int,' \
-                   f' {columns[2]} datetime,' \
-                   f' {columns[3]} datetime,' \
-                   f' {columns[4]} money,' \
-                   f' {columns[5]} money)'
-    sql_open_now = cursor.execute(create_query)
-    conn.commit()
-
-# Преобразуем данные в подходящиц формат
-    for i in range(len(list_data)):
-        list_data[i][2] = datetime.strftime(datetime.strptime(str(list_data[i][2])[:10], '%Y-%m-%d'), '%Y-%m-%d')
-        list_data[i][3] = datetime.strftime(datetime.strptime(str(list_data[i][3])[:10], '%Y-%m-%d'), '%Y-%m-%d')
-        insert_execute = f'insert into Result_kontur_ekstern (' \
-                         f'{columns[0]}, {columns[1]}, {columns[2]}, {columns[3]}, {columns[4]}, {columns[5]})' \
-                         f'values ' \
-                         f'({list_data[i][0]}, {list_data[i][1]}, {list_data[i][2]},' \
-                         f' {list_data[i][3]}, {list_data[i][4]}, {list_data[i][5]})'
-        insert_execute = str(insert_execute)
-        cursor.execute(insert_execute)
-        conn.commit()
-    print('В базе данных проверьте таблицу Result_kontur_ekstern в ней результат работы скрипта')
+print('В базе данных проверьте таблицу Result_kontur_ekstern в ней результат работы скрипта')
